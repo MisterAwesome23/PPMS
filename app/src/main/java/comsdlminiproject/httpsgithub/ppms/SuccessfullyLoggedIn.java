8e1898bf47a93e3bbbc52e5700e887c974975f59
MisterAwesome23 <36367483+MisterAwesome23@users.noreply.github.com>
2018-09-24 18:49:13 +0530

    SignuUp Page created. LogIn Page Created. Successfully logged in page created. Version 2.

--- app/src/main/java/comsdlminiproject/httpsgithub/ppms/SuccessfullyLoggedIn.java
+++ app/src/main/java/comsdlminiproject/httpsgithub/ppms/SuccessfullyLoggedIn.java
@@ -1,13 +1,30 @@
 package comsdlminiproject.httpsgithub.ppms;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccessfullyLoggedIn extends AppCompatActivity {
 
+
+    Button btnOpenMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successfully_logged_in);
+
+        btnOpenMap= findViewById(R.id.btnOpenMap);
+        btnOpenMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Nearest petrol pump"));
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
