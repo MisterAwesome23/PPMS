2c0b6db9c0c02caec5d5544542486ca244d18735
MisterAwesome23 <36367483+MisterAwesome23@users.noreply.github.com>
2018-09-25 02:13:45 +0530

    LogOut button added.

--- app/src/main/java/comsdlminiproject/httpsgithub/ppms/SuccessfullyLoggedIn.java
+++ app/src/main/java/comsdlminiproject/httpsgithub/ppms/SuccessfullyLoggedIn.java
@@ -4,13 +4,43 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SuccessfullyLoggedIn extends AppCompatActivity {
 
 
     Button btnOpenMap,btnGetLatestPetrolPrice;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logoutmenu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.LOGOUT:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(),ActivityLogIn.class));
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +64,13 @@ public class SuccessfullyLoggedIn extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
 
 
     }
